9c3a91b8974c9c0db25144d15d27c210e2e33329
tangxue <xue.tang@u-xian.com>
2017-03-15 20:44:42 +0800

    送达时间

--- src/components/submitOrder/submitOrder.vue
+++ src/components/submitOrder/submitOrder.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="submitOrder">
     <div class="submitOrder-wrapper">
-      <section class="address-wrapper">
+      <section class="address-wrapper" @click="gotoAddList">
         <i class="uxwm-iconfont icon_Location"></i>
         <div class="address-not">
           请添加一个收获地址
@@ -11,10 +11,18 @@
       <section class="serviceTime-wrapper">
         <i class="uxwm-iconfont icon_time"></i>
         <span class="text">送达时间</span>
-        <div class="time-wrapper">
-          <span class="title">尽快送达</span>
-          <span class="time">预计12:30</span>
+        <div class="time-wrapper" @click="timeSelect">
+          <span class="title" v-if="selected === 0">尽快送达</span>
+          <span class="time" v-if="selected === 0">预计{{estimateTime | formatDate}}</span>
+          <span class="timeItem" v-if="selected > 0">{{options[selected] | formatDate}}</span>
         </div>
+        <select class="time-select" v-model="selected" placeholder="请选择">
+          <option class="select-item" v-for="(item, index) in options" :value="index">
+            <span class="title" v-if="index === 0">尽快送达 &nbsp|&nbsp</span>
+            <span class="time" v-if="index === 0"> 预计{{item | formatDate}}</span>
+            <span v-if="index > 0">{{item | formatDate}}</span>
+          </option>
+        </select>
       </section>
       <section class="orderDetail-wrapper">
         <div class="title">订单详情</div>
@@ -51,10 +59,57 @@
 </template>
 
 <script type="text/ecmascript-6">
+  import {formatDate} from '../../common/js/date'
   export default {
+    data() {
+      return {
+        options: [], // 时间数组
+        selected: 0, // 默认index
+        estimateTime: new Date(new Date().setMinutes(new Date().getMinutes() + 30)), // 预计时间
+        endTime: 1489586400000, // 结束时间
+        key: 45
+      }
+    },
+    created() {
+      this.options.push(this.estimateTime)
+
+      // 预计默认时间
+      let orderTaP = new Date().setMinutes(new Date().getMinutes() + 30)
+
+      // 最后一次时间
+      this.endTime = new Date(this.endTime).setMinutes(-15)
+
+      // 开始第一次时间
+      let oneTimeIndex = Math.floor(this.estimateTime.getMinutes() / 15)
+      if (oneTimeIndex === 0) {
+        this.options.push(new Date(orderTaP).setMinutes(15))
+      } else if (oneTimeIndex === 1) {
+        this.options.push(new Date(orderTaP).setMinutes(30))
+      } else if (oneTimeIndex === 2) {
+        this.options.push(new Date(orderTaP).setMinutes(45))
+      } else if (oneTimeIndex === 3) {
+        new Date(new Date(orderTaP).setUTCMinutes(0)).setHours(new Date(orderTaP).getHours() + 1)
+        this.options.push(new Date(new Date(orderTaP).setUTCMinutes(0)).setHours(new Date(orderTaP).getHours() + 1))
+      }
+      let oncTime = this.options[1]
+      while (oncTime < this.endTime) {
+        this.options.push(oncTime += 900000)
+      }
+    },
     methods: {
+      // 去支付
       gotoPay() {
         this.$router.push({path: '/submitPay'})
+      },
+      // 去地址列表
+      gotoAddList() {
+        this.$router.push({path: '/addList'})
+      }
+    },
+    filters: {
+      formatDate(time) {
+        let date = new Date(time)
+        return formatDate(date, 'yyyy-MM-dd hh:mm')
       }
     }
   }
@@ -96,6 +151,7 @@
   }
 
   .serviceTime-wrapper {
+    position: relative;
     box-sizing: border-box;
     margin-bottom: 14px;
     padding: 0 14px;
@@ -126,8 +182,9 @@
     display: inline-block;
     float: right;
     color: #ff8923;
-    font-size: 0px;
+    font-size: 14px;
     font-weight: 500;
+    letter-spacing: 1px;
   }
 
   .serviceTime-wrapper .time-wrapper .title {
@@ -142,6 +199,27 @@
     display: inline-block;
     padding-left: 10px;
     font-size: 14px;
+  }
+
+  .serviceTime-wrapper .time-wrapper .timeItem {
+    letter-spacing: 2px;
+  }
+
+  .serviceTime-wrapper .time-select {
+    position: absolute;
+    top: 0;
+    right: 0;
+    width: 175px;
+    height: 39px;
+    color: #ff8923;
+    font-size: 14px;
+    font-weight: 500;
+    border: none;
+    opacity: 0;
+  }
+
+  .serviceTime-wrapper .time-select .select-item {
+    text-align: center;
   }
 
   .orderDetail-wrapper {
@@ -266,5 +344,10 @@
     font-weight: 700;
     background: #ff8932;
     letter-spacing: 1px;
+  }
+
+  .time-select {
+    width: 100px;
+    font-size: 12px;
   }
 </style>
