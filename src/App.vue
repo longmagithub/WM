f97b1d320196869a5c9380c259cadc7b93863177
tangxue <xue.tang@u-xian.com>
2017-03-23 17:57:59 +0800

    打包

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
 <template>
   <div id="app">
     <transition name="fade" mode="out-in">
-      <router-view class="view"></router-view>
+      <router-view class="view"><index></index></router-view>
     </transition>
-    <div>测试12123</div>
+    <!--<div>测试12123</div>-->
   </div>
 </template>
+<script type="text/ecmascript-6">
+  import index from './components/index/index.vue'
+  export default {
+    components: {
+      index
+    }
+  }
+</script>
 <style lang="sass">
   @import './style/base.scss';
 </style>
