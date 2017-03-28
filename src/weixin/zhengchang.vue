0c967bf792a487d5d5771ec0bd8d37180e3ca801
tangxue <xue.tang@u-xian.com>
2017-03-28 20:16:25 +0800

    授权

--- src/weixin/zhengchang.vue
+++ src/weixin/zhengchang.vue
@@ -5,6 +5,8 @@
   </div>
 </template>
 <script>
+  import {urlParse} from '../common/js/util'
+
   export default {
     data () {
       return {
@@ -16,6 +18,8 @@
       this.url = window.location.href
       if (this.url.indexOf('code') < 0) {
         this.to()
+      } else {
+        this.getOpenId()
       }
     },
     methods: {
@@ -23,6 +27,32 @@
         const oauthCallbackUrl = encodeURIComponent('http://newpay.tunnel.qydev.com/VAOrderH5/#/zhengchang1')
         const oauthJumpUrl = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx980e7bb068f0b763&redirect_uri=${oauthCallbackUrl}&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect`
         window.location.href = oauthJumpUrl
+      },
+      getOpenId () {
+        const data = {
+          code: urlParse().code,
+          type: 2 // 授权类型：1静默授权；2用户授权
+        }
+        const api = '/mp/authority/customer'
+        this.axios.post(api, data).then((res) => {
+          const d = res.data
+          if (d.success) {
+            console.log(d)
+            this.jump(d.data.customerId)
+          }
+        }, (errorRes) => {
+          console.log(errorRes)
+        })
+      },
+      // 如果有code 跳转页面
+      jump (customerId) {
+        this.$router.replace({
+          path: '/index',
+          query: {
+            'shopId': 'ca2939cf-f42f-402f-8b75-53283431ee68',
+            'customerId': customerId
+          }
+        })
       }
     }
   }
