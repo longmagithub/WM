1a64d2b4af38ebd3fa167f1ad098bbb99b40073f
tangxue <xue.tang@u-xian.com>
2017-04-01 11:23:04 +0800

    test

--- src/components/submitPay/submitPay.vue
+++ src/components/submitPay/submitPay.vue
@@ -120,12 +120,12 @@
       sendWxSDK (data) {
         console.log(data)
         wx.chooseWXPay({
-          appId: data.appId,
-          timeStamp: Math.floor(data.timeStamp / 1000),
+          appId: 'wx4340e30a4fa4b63b',
+          timeStamp: Math.floor(data.timeStamp / 1000) + '',
           // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
           nonceStr: data.nonceStr, // 支付签名随机串，不长于 32 位
           package: data.packageDetail, // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=***）
-          signType: data.signType, // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'
+          signType: 'MD5', // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'
           paySign: data.paySign, // 支付签名
           success: function (res) {
             // 支付成功后的回调函数
