b38719d279423f0cdbe13e1d28063c6b5bd5fd8a
tangxue <xue.tang@u-xian.com>
2017-04-01 20:19:41 +0800

    test

--- src/components/submitPay/submitPay.vue
+++ src/components/submitPay/submitPay.vue
@@ -64,7 +64,7 @@
       },
       //  获取 wxconfig
       getWxConfig() {
-        const that = this
+//        const that = this
         if (!this.isTime) {
           this.toastShow = true
           this.toastText = '订单已超时'
@@ -88,18 +88,18 @@
             .then((res) => {
               res = res.data
               if (res.success === SUCCESS_OK) {
-                wx.config({
-                  debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-                  appId: res.data.appId, // 必填，公众号的唯一标识
-                  timestamp: res.data.timestamp, // 必填，生成签名的时间戳
-                  nonceStr: res.data.nonceStr, // 必填，生成签名的随机串
-                  signature: res.data.signature, // 必填，签名，见附录1
-                  jsApiList: ['chooseWXPay'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
-                })
-                wx.ready(() => {
-                  window.alert('**去支付喽**')
-                  that.weChatPay()
-                })
+                /* wx.config({
+                 debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+                 appId: res.data.appId, // 必填，公众号的唯一标识
+                 timestamp: res.data.timestamp, // 必填，生成签名的时间戳
+                 nonceStr: res.data.nonceStr, // 必填，生成签名的随机串
+                 signature: res.data.signature, // 必填，签名，见附录1
+                 jsApiList: ['chooseWXPay'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+                 })
+                 wx.ready(() => {
+                 window.alert('**去支付喽**')
+                 that.weChatPay()
+                 }) */
               } else {
                 this.toastShow = true
                 this.toastText = '订单支付失败，请稍候重试'
