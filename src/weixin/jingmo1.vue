caf50c1a897fe50a4a8abdcd1a23ee3b4774a7c7
tangxue <xue.tang@u-xian.com>
2017-04-08 21:13:51 +0800

    test

--- src/weixin/jingmo1.vue
+++ src/weixin/jingmo1.vue
@@ -44,7 +44,8 @@
         })
       },
       jump (customerId) {
-        window.location.href = 'http://newpay.tunnel.qydev.com/VAOrderH5/#/index?shopId=' + this.shopId + '&customerId=' + customerId
+        window.location.href = 'http://newpay.tunnel.qydev.com/VAOrderH5/?#/index?shopId=' + this.shopId +
+          '&customerId=' + customerId
       }
     }
   }
