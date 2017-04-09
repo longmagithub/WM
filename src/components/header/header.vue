7bf5f3ebaa0a4293c1983294f470d6b657ea7c13
tangxue <xue.tang@u-xian.com>
2017-04-09 12:05:49 +0800

    test

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -11,8 +11,8 @@
           <div class="content">
             <div class="title-name">{{seller.name}}</div>
             <!-- 描述 -->
-            <div class="description">{{seller.dispatching.name}}/{{seller.dispatching.duration}}分钟到达<span
-              v-if="seller.dispatching.fees.length">/满{{seller.dispatching.fees[0].price}}{{seller.dispatching.fees[0].fee | fees}}</span>
+            <!--<div class="description">{{seller.dispatching.name}}/{{seller.dispatching.duration}}分钟到达<span-->
+              <!--v-if="seller.dispatching.fees.length">/满{{seller.dispatching.fees[0].price}}{{seller.dispatching.fees[0].fee | fees}}</span>-->
             </div>
             <div class="bulletin" v-if="seller.notice">公告：{{seller.notice}}</div>
           </div>
