918e34e4a723d4d6168998c75d094143454863eb
tangxue <xue.tang@u-xian.com>
2017-04-09 12:14:25 +0800

    test

--- src/weixin/jingmo.vue
+++ src/weixin/jingmo.vue
@@ -27,8 +27,7 @@
     methods: {
       to () {
         const oauthCallbackUrl =
-          encodeURIComponent('http://newpay.tunnel.qydev.com/VAOrderH5/#/jingmo1?T=' +
-            Date.parse(new Date()) / 10000) + '&shopId=' + this.shopId
+          encodeURIComponent('http://newpay.tunnel.qydev.com/VAOrderH5/#/jingmo1?shopId=' + this.shopId)
         const oauthJumpUrl = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx96f6daa5f8a71039&redirect_uri=${oauthCallbackUrl}&response_type=code&scope=snsapi_base&state=STATE#wechat_redirect`
         window.location.href = oauthJumpUrl
       }
