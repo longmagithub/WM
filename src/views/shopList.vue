db57235c4814604c7c6d2bbb9984fce06c2f29e4
tangxue <xue.tang@u-xian.com>
2017-04-11 13:23:48 +0800

    test

--- src/views/shopList.vue
+++ src/views/shopList.vue
@@ -14,7 +14,7 @@
           <span class="tiem">30分钟</span>
         </div>
       </li>
-      <li class="list-item">
+      <li class="list-item" @click="goIndex('631af368-3da3-4278-8f1c-42f55d856e6d')">
         <div class="logo">
           <img
             src="http://imagewm.test.u-xian.com/image/shop/e452573e-69df-4912-ad96-8b3f4ad52d1f/1491718758273shopImage.JPEG"
@@ -27,7 +27,7 @@
           <span class="tiem">28分钟</span>
         </div>
       </li>
-      <li class="list-item">
+      <li class="list-item" @click="goIndex('8a124c47-eff3-4bbc-8053-c01f72174005')">
         <div class="logo">
           <img
             src="http://imagewm.test.u-xian.com/image/shop/af8b7bfa-34b8-4266-accf-1dd64eff580d/1491674516791shopImage.JPEG"
@@ -40,7 +40,7 @@
           <span class="tiem">28分钟</span>
         </div>
       </li>
-      <li class="list-item">
+      <li class="list-item" @click="goIndex('8d68095c-4ca8-40d0-b3a0-c40dac778c17')">
         <div class="logo">
           <img
             src="http://imagewm.test.u-xian.com/image/shop/c5e25f9d-b7d4-4da8-8652-1ddfebae7077/1491674174626shopImage.JPEG"
@@ -53,7 +53,7 @@
           <span class="tiem">22分钟</span>
         </div>
       </li>
-      <li class="list-item">
+      <li class="list-item" @click="goIndex('9480c029-a45d-479e-a3c3-74ed9c65d54d')">
         <div class="logo">
           <img src="http://imagewm.test.u-xian.com/image/shop/1491686621833shopImage.JPEG" width="48px" height="48px">
         </div>
@@ -64,7 +64,7 @@
           <span class="tiem">32分钟</span>
         </div>
       </li>
-      <li class="list-item">
+      <li class="list-item" @click="goIndex('af8b7bfa-34b8-4266-accf-1dd64eff580d')">
         <div class="logo">
           <img
             src="http://imagewm.test.u-xian.com/image/shop/8d68095c-4ca8-40d0-b3a0-c40dac778c17/1491674343847shopImage.JPEG"
@@ -77,7 +77,7 @@
           <span class="tiem">30分钟</span>
         </div>
       </li>
-      <li class="list-item">
+      <li class="list-item" @click="goIndex('c1a2c562-b2cc-46c2-9e10-47cc8552ca4e')">
         <div class="logo">
           <img
             src="http://imagewm.test.u-xian.com/image/shop/9480c029-a45d-479e-a3c3-74ed9c65d54d/1491686827765shopImage.JPEG"
@@ -90,7 +90,7 @@
           <span class="tiem">23分钟</span>
         </div>
       </li>
-      <li class="list-item">
+      <li class="list-item" @click="goIndex('c1c8dafb-0d9e-4722-b7d8-a540d235e30c')">
         <div class="logo">
           <img
             src="http://imagewm.test.u-xian.com/image/shop/7da10ea0-91ea-4db2-852f-b46afb63ada6/1491673784318shopImage.JPEG"
@@ -103,7 +103,7 @@
           <span class="tiem">34分钟</span>
         </div>
       </li>
-      <li class="list-item">
+      <li class="list-item" @click="goIndex('c5e25f9d-b7d4-4da8-8652-1ddfebae7077')">
         <div class="logo">
           <img
             src="http://imagewm.test.u-xian.com/image/shop/c1c8dafb-0d9e-4722-b7d8-a540d235e30c/1491674039775shopImage.JPEG	"
@@ -116,7 +116,7 @@
           <span class="tiem">38分钟</span>
         </div>
       </li>
-      <li class="list-item">
+      <li class="list-item" @click="goIndex('d8438de5-d90d-431d-bd68-0d0a85f4401a')">
         <div class="logo">
           <img
             src="http://imagewm.test.u-xian.com/image/shop/4637cede-37aa-4648-94f5-2799f93508c9/1491718406275shopImage.JPEG	"
@@ -129,7 +129,7 @@
           <span class="tiem">35分钟</span>
         </div>
       </li>
-      <li class="list-item">
+      <li class="list-item" @click="goIndex('de74f04c-fb3f-4b04-af59-f74eca53d653')">
         <div class="logo">
           <img src="http://imagewm.u-xian.com/image/shop/1491817065330shopImage.JPEG" width="48px" height="48px">
         </div>
@@ -140,7 +140,7 @@
           <span class="tiem">28分钟</span>
         </div>
       </li>
-      <li class="list-item">
+      <li class="list-item" @click="goIndex('e452573e-69df-4912-ad96-8b3f4ad52d1f')">
         <div class="logo">
           <img
             src="http://imagewm.test.u-xian.com/image/shop/d8438de5-d90d-431d-bd68-0d0a85f4401a/1491718589749shopImage.JPEG	"
