915a969c1b4d443551de356e1569737cbc98f268
tangxue <xue.tang@u-xian.com>
2017-04-12 17:37:31 +0800

    tset

--- src/weixin/jingmo.vue
+++ src/weixin/jingmo.vue
@@ -21,18 +21,18 @@
       this.to()
       console.log('jingmo')
     },
-    mounted () {
-      this.url = window.location.href
-      if (this.url.indexOf('code') < 0) {
-      }
-    },
+//    mounted () {
+//      this.url = window.location.href
+//      if (this.url.indexOf('code') < 0) {
+//      }
+//    },
     methods: {
       to () {
         const oauthCallbackUrl =
           encodeURIComponent('http://newpay.tunnel.qydev.com/VAOrderH5/#/jingmo1')
         const oauthJumpUrl = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx96f6daa5f8a71039&redirect_uri=${oauthCallbackUrl}&response_type=code&scope=snsapi_base&state=STATE#wechat_redirect`
         // 跳转授权 浏览器不保留记录
-        // window.location.replace(oauthJumpUrl)
+//         window.location.replace(oauthJumpUrl)
         window.location.href = oauthJumpUrl
       }
     }
