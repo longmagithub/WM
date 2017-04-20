0a9f1e922750f5f4a0a32ae0cf8a7cf50133f958
tangxue <xue.tang@u-xian.com>
2017-04-20 18:15:42 +0800

    test

--- src/components/index/index.vue
+++ src/components/index/index.vue
@@ -127,7 +127,7 @@
               appId: res.data.appId, // 必填，公众号的唯一标识
               timestamp: res.data.timestamp, // 必填，生成签名的时间戳
               nonceStr: res.data.nonceStr, // 必填，生成签名的随机串
-              signature: res.data.signature.toLowerCase(), // 必填，签名，见附录1
+              signature: res.data.signature, // 必填，签名，见附录1
               jsApiList: ['onMenuShareTimeline', 'onMenuShareAppMessage', 'onMenuShareQQ', 'onMenuShareWeibo',
                 'onMenuShareQZone'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
             })
