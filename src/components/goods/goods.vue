055028adf3a1158eef2e412a76c0360fa86c4e3b
tangxue <xue.tang@u-xian.com>
2017-04-21 11:38:15 +0800

    etst

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -19,8 +19,8 @@
             <li class="food-item"
                 v-for="(food, foodIndex) in item.dishList" :key="foodIndex">
               <div class="icon">
-                <!--<img v-if="food.imageUrl" :src="food.imageUrl" width="52px" height="52px">-->
-                <!--<img v-else src="../../assets/logo.png" width="52px" height="52px">-->
+                <img v-if="food.imageUrl" :src="food.imageUrl" width="52px" height="52px">
+                <img v-else src="../../assets/logo.png" width="52px" height="52px">
               </div>
               <div class="content">
                 <div class="name_box"><span class="name">{{food.name}}</span></div>
