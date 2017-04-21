e783f61443ecb3460e453f848df910d9736b1fc5
tangxue <xue.tang@u-xian.com>
2017-04-21 10:26:56 +0800

    test

--- src/components/wxshare.vue
+++ src/components/wxshare.vue
@@ -25,12 +25,12 @@
                 'onMenuShareQZone'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
             })
             wx.ready(function () {
-              window.alert('213123123')
+              console.log('我是 ready ')
               // config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
             })
             wx.error(function (data) {
               window.alert(data)
-              console.log('error')
+              console.log('我是 error')
               // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
             })
           }
