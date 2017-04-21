260f7f656226ccad1daa819a70918079b2390184
tangxue <xue.tang@u-xian.com>
2017-04-21 15:24:50 +0800

    etes

--- src/components/index/index.vue
+++ src/components/index/index.vue
@@ -23,7 +23,7 @@
         </div>
       </div>
     </div>
-    <wxshare></wxshare>
+    <!--<wxshare></wxshare>-->
   </div>
 </template>
 
@@ -76,25 +76,21 @@
       // 分享按钮
       goShare() {
         console.log('我是胖子')
-        wx.ready(function () {
-          console.log('我是 ready ')
-          wx.onMenuShareAppMessage({
-            title: '分享标题-大明湖', // 分享标题
-            desc: '分享描述-大明湖畔夏雨荷', // 分享描述
-            link: 'http://newpay.tunnel.qydev.com/VAOrderH5/#/share', // 分享链接
-            imgUrl: '', // 分享图标
-            type: '', // 分享类型,music、video或link，不填默认为link
-            dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
-            success: function () {
-              window.alert('分享成功')
-              // 用户确认分享后执行的回调函数
-            },
-            cancel: function () {
-              window.alert('取消分享')
-              // 用户取消分享后执行的回调函数
-            }
-          })
-          // config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
+        wx.onMenuShareAppMessage({
+          title: '分享标题-大明湖', // 分享标题
+          desc: '分享描述-大明湖畔夏雨荷', // 分享描述
+          link: 'http://newpay.tunnel.qydev.com/VAOrderH5/#/share', // 分享链接
+          imgUrl: '', // 分享图标
+          type: '', // 分享类型,music、video或link，不填默认为link
+          dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
+          success: function () {
+            window.alert('分享成功')
+            // 用户确认分享后执行的回调函数
+          },
+          cancel: function () {
+            window.alert('取消分享')
+            // 用户取消分享后执行的回调函数
+          }
         })
       },
       // 商家信息
--- src/components/wxshare.vue
+++ src/components/wxshare.vue
@@ -21,33 +21,33 @@
               timestamp: res.data.timestamp, // 必填，生成签名的时间戳
               nonceStr: res.data.nonceStr, // 必填，生成签名的随机串
 //              signature: 'a1e4891d25181dd5ee1903f60782de4bdd16ce49', // 必填，签名，见附录1
-              signature: res.data.signature.toLowerCase(), // 必填，签名，见附录1
-              jsApiList: ['onMenuShareTimeline', 'onMenuShareAppMessage']
+              signature: res.data.signature, // 必填，签名，见附录1
+              jsApiList: ['onMenuShareAppMessage']
             })
-            wx.ready(function () {
-              console.log('我是 ready ')
-              wx.onMenuShareAppMessage({
-                title: '分享标题-大明湖', // 分享标题
-                desc: '分享描述-大明湖畔夏雨荷', // 分享描述
-                link: 'http://newpay.tunnel.qydev.com/VAOrderH5/#/share', // 分享链接
-                imgUrl: '', // 分享图标
-                type: '', // 分享类型,music、video或link，不填默认为link
-                dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
-                success: function () {
-                  window.alert('分享成功')
-                  // 用户确认分享后执行的回调函数
-                },
-                cancel: function () {
-                  window.alert('取消分享')
-                  // 用户取消分享后执行的回调函数
-                }
-              })
-              // config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
-            })
-            wx.error(function (data) {
-              console.log('我是 error')
-              // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
-            })
+//            wx.ready(function () {
+//              console.log('我是 ready ')
+//              wx.onMenuShareAppMessage({
+//                title: '分享标题-大明湖', // 分享标题
+//                desc: '分享描述-大明湖畔夏雨荷', // 分享描述
+//                link: 'http://newpay.tunnel.qydev.com/VAOrderH5/#/share', // 分享链接
+//                imgUrl: '', // 分享图标
+//                type: '', // 分享类型,music、video或link，不填默认为link
+//                dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
+//                success: function () {
+//                  window.alert('分享成功')
+//                  // 用户确认分享后执行的回调函数
+//                },
+//                cancel: function () {
+//                  window.alert('取消分享')
+//                  // 用户取消分享后执行的回调函数
+//                }
+//              })
+//              // config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
+//            })
+//            wx.error(function (data) {
+//              console.log('我是 error')
+//              // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
+//            })
           }
         })
       }
