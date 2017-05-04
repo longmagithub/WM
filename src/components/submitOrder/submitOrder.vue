0dfbf01ba08d3dba789a691e9107efcfa5d7fb46
Tangxve <xue.tang@u-xian.com>
2017-05-04 11:04:46 +0800

    测试 添加新接口（距离阶梯配送费 ）

--- src/components/submitOrder/submitOrder.vue
+++ src/components/submitOrder/submitOrder.vue
@@ -157,7 +157,7 @@
       // 商户信息
       this.shopInfo = getStore('shopInfo')
       this.packPrice = parseFloat(getStore('userPrice')[0].toFixed(2))
-      this.feesPrice = parseFloat(getStore('userPrice')[1].toFixed(2))
+//      this.feesPrice = parseFloat(getStore('userPrice')[1].toFixed(2))
       this.allPrice = parseFloat(getStore('userPrice')[2].toFixed(2))
       // 默认地址
       this.getAddRess(this.addressId)
@@ -264,6 +264,7 @@
             res = res.data
             if (res.success) {
               this.addRess = res.data
+              this.getDispatchPrice(res.data.distance)
             }
           })
         } else {  // 默认地址
@@ -275,6 +276,7 @@
             res = res.data
             if (res.success === true) {
               this.addRess = res.data
+              this.getDispatchPrice(res.data.distance)
             }
           })
         }
@@ -305,6 +307,25 @@
               }
             })
           })
+        })
+      },
+      // 阶梯配送费
+      getDispatchPrice(userPosition) {
+        const data = {
+          sessionId: this.customerId,
+          shopId: this.shopId
+        }
+        this.axios.get(`br/dispatch/price${this.PublicJs.createParams(data)}`).then((res) => {
+          res = res.data
+          if (res.success) {
+            res.data.dispatchPriceList.forEach((item) => {
+              if (item.startDistance >= userPosition < item.endDistance) {
+                this.feesPrice = item.price
+              } else {
+                this.toggleToast(1, '地址超出配送范围，请重新选择')
+              }
+            })
+          }
         })
       },
       // 提交订单
