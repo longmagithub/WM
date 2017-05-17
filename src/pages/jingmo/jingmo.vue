1ea79dd553311146307bdb84df210ce63ed70648
Tangxve <xue.tang@u-xian.com>
2017-05-17 17:17:47 +0800

    百度跨域出现问题

--- src/pages/jingmo/jingmo.vue
+++ src/pages/jingmo/jingmo.vue
@@ -1,5 +1,4 @@
 <template>
-  <wxshare></wxshare>
 </template>
 <script>
   import {urlParse, setStore} from '../../common/utils/util'
@@ -64,20 +63,7 @@
             })
             let setStoreContetn = {custId: res.data}
             setStore('shopList', setStoreContetn)
-            wx.ready(() => {
-              wx.getLocation({
-                type: 'gcj02', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
-                success: function (res) {
-                  window.alert(res)
-                  this.getBaiDuMap(res)
-//                  var latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
-//                  var longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
-//                  var speed = res.speed; // 速度，以米/每秒计
-//                  var accuracy = res.accuracy; // 位置精度
-                }
-              })
-            })
-//            this.goIndex(res.data[0].shopId, custId)
+            this.goIndex(res.data[0].shopId, custId)
           }
         })
       },
