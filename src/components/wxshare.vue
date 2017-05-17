12dd89626cc351eb012afb05a2137fe7ff51d80c
Tangxve <xue.tang@u-xian.com>
2017-05-17 22:02:30 +0800

    测试地理位置

--- src/components/wxshare.vue
+++ src/components/wxshare.vue
@@ -772,6 +772,7 @@
           coord_type: 'gcj02' // 坐标类型
         }
         this.$http.jsonp(`http://api.map.baidu.com/routematrix/v2/riding${this.PublicJs.createParams(data)}`).then((res) => {
+          window.alert(res)
           res = res.data
           console.log(res)
           res.result.forEach((item, index) => {
@@ -790,6 +791,8 @@
           }
           setStore('shopList', this.shopListArr)
 //          console.log(JSON.stringify(this.shopListArr))
+        }, (err) => {
+          window.alert(err)
         })
       }
     }
