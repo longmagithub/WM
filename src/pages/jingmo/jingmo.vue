97e2b5aadb13499431b5d76ad89be8e3d9c5f8b9
Tangxve <xue.tang@u-xian.com>
2017-05-21 23:44:28 +0800

    爆款添加到购物车后动态满减隐藏

--- src/pages/jingmo/jingmo.vue
+++ src/pages/jingmo/jingmo.vue
@@ -1,7 +1,8 @@
 <template>
 </template>
 <script>
-  import {urlParse, setStore} from '../../common/utils/util'
+//  import {urlParse, setStore} from '../../common/utils/util'
+  import {urlParse} from '../../common/utils/util'
   import wxshare from '../../components/wxshare.vue'
   export default {
     data () {
@@ -9,10 +10,12 @@
         msg: '静默授权',
         url: '',
         code: '',
-        customerId: ''
+        customerId: '',
+        shopId: ''
       }
     },
     created() {
+      this.shopId = urlParse().shopId
       this.url = window.location.href
       if (this.url.indexOf('code') < 0) {
         this.to()
@@ -25,7 +28,9 @@
           res = res.data
           if (res.success) {
             this.customerId = res.data.customerId
-            this.getShopList(res.data.customerId)
+            this.shopId
+            this.goIndex(this.shopId, this.customerId)
+//            this.getShopList(res.data.customerId)
           }
         })
       }
@@ -40,32 +45,32 @@
 //        window.location.href = oauthJumpUrl
       },
       // 获取列表
-      getShopList(custId) {
-        const data = {
-          customerId: custId,
-          pageSize: 10000,
-          pageNumber: 1,
-          longitude: 0, // 经度
-          latitude: 0, // 维度
-          discounts: [], // uxwm 满减
-          thirdDiscounts: [] // 其他平台满减
-        }
-        this.axios.get(`/br/shop/list${this.PublicJs.createParams(data)}`).then((res) => {
-          res = res.data
-          if (res.success) {
-            res.data.forEach((data) => {
-              data.discounts = data.discounts.reverse()
-              data.thirdDiscounts = data.thirdDiscounts.reverse()
-              // 添加 图片分割
-              if (data.logo) {
-                data.logo = data.logo + '?x-oss-process=image/resize,m_fill,h_100,w_100'
-              }
-            })
-            setStore('shopList', res.data)
-            this.goIndex(res.data[0].shopId, custId)
-          }
-        })
-      },
+//      getShopList(custId) {
+//        const data = {
+//          customerId: custId,
+//          pageSize: 10000,
+//          pageNumber: 1,
+//          longitude: 0, // 经度
+//          latitude: 0, // 维度
+//          discounts: [], // uxwm 满减
+//          thirdDiscounts: [] // 其他平台满减
+//        }
+//        this.axios.get(`/br/shop/list${this.PublicJs.createParams(data)}`).then((res) => {
+//          res = res.data
+//          if (res.success) {
+//            res.data.forEach((data) => {
+//              data.discounts = data.discounts.reverse()
+//              data.thirdDiscounts = data.thirdDiscounts.reverse()
+//              // 添加 图片分割
+//              if (data.logo) {
+//                data.logo = data.logo + '?x-oss-process=image/resize,m_fill,h_100,w_100'
+//              }
+//            })
+//            setStore('shopList', res.data)
+//            this.goIndex(res.data[0].shopId, custId)
+//          }
+//        })
+//      },
       goIndex(shopId, custId) {
         window.location.replace('http://newpay.tunnel.qydev.com/VAOrderH5/?#/index?customerId=' + custId + '&shopId=' +
           shopId + '&t=' + Date.parse(new Date()))
