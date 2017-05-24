866220b39d22e447773cc813a66e9ae3b64e9e91
Tangxve <xue.tang@u-xian.com>
2017-05-24 11:07:25 +0800

    修改定位

--- src/pages/shopList/shopList.vue
+++ src/pages/shopList/shopList.vue
@@ -73,6 +73,7 @@
 //        console.log('12311231312434342324243')
         if (navigator.geolocation) {
           navigator.geolocation.getCurrentPosition((location) => {
+            window.alert('获取成功')
             let coords = location.coords
             let longitude = coords.longitude // 经度
             let latitude = coords.latitude // 纬度
@@ -80,22 +81,22 @@
           }, (error) => {
             switch (error.code) {
               case error.PERMISSION_DENIED:
-                window.alert('定位失败,用户拒绝请求地理定位')
+                window.alert('定位失败,用户拒绝请求地理定位11111')
                 break
               case error.POSITION_UNAVAILABLE:
-                window.alert('定位失败,位置信息是不可用')
+                window.alert('定位失败,位置信息是不可用22222')
                 break
               case error.TIMEOUT:
-                window.alert('定位失败,请求获取用户位置超时')
+                window.alert('定位失败,请求获取用户位置超时33333')
                 break
               case error.UNKNOWN_ERROR:
-                window.alert('定位失败,定位系统失效')
+                window.alert('定位失败,定位系统失效44444')
                 break
             }
             this.getShopList(this.customerId, 0, 0)
           })
         } else {
-          window.alert('无法获取到您的地理定位')
+          window.alert('无法获取到您的地理定位55555')
         }
       },
       // 请求门店列表
