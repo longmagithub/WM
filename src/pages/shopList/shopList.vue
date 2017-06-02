a7a16887d6ccf79cadcd380a31c02ffd8b3f4bf9
Tangxve <xue.tang@u-xian.com>
2017-06-02 17:03:43 +0800

    test

--- src/pages/shopList/shopList.vue
+++ src/pages/shopList/shopList.vue
@@ -152,18 +152,18 @@
       },
       // 去首页
       goIndex(id, isDistance) {
-        setTimeout(() => {
-          window.location.href =
-            'http://newpay.tunnel.qydev.com/VAOrderH5/?#/index?shopId=' + id + '&isDistance=' + isDistance + '&customerId=' + this.customerId + '&T=' + Date.parse(new Date()) / 1000
-        }, 500)
-//        this.$router.push({
-//          path: '/index',
-//          query: {
-//            shopId: id,
-//            customerId: this.customerId,
-//            isDistance: isDistance ? 1 : 0
-//          }
-//        })
+//        setTimeout(() => {
+//          window.location.href =
+//            'http://newpay.tunnel.qydev.com/VAOrderH5/?#/index?shopId=' + id + '&isDistance=' + isDistance + '&customerId=' + this.customerId + '&T=' + Date.parse(new Date()) / 1000
+//        }, 500)
+        this.$router.push({
+          path: '/index',
+          query: {
+            shopId: id,
+            customerId: this.customerId,
+            isDistance: isDistance ? 1 : 0
+          }
+        })
       }
     }
   }
